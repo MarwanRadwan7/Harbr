4a0e24590b48e0e8ae7219c87b7e1629409c73a2
MarwanRadwan7 <marwan.radwan.dev@gmail.com>
2026-04-30 00:49:56 +0300

    refactor app startup

---
+++ src/test/java/com/harbr/HarbrApplicationTests.java
@@ -0,0 +1,12 @@
+package com.harbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarbrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
