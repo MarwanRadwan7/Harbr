90e4b249121c16b2fd87f33050a063728dd21486
MarwanRadwan7 <marwan.radwan.dev@gmail.com>
2026-05-01 18:44:51 +0300

    refactor: move HarbrApplication to com.harbr package and add JPA auditing, repository scanning, and OAuth2 exclusion

--- src/main/java/com/harbr/HarbrApplication.java
+++ src/main/java/com/harbr/HarbrApplication.java
@@ -2,15 +2,18 @@ package com.harbr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableCaching
-@EnableScheduling
+@SpringBootApplication(excludeName = "org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration")
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.harbr")
 public class HarbrApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HarbrApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HarbrApplication.class, args);
+	}
+
 }
